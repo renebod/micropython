5ad8be46849b94afd3ecf2ab47a3a00c54f48edc
renebod <rene.bod@mac.com>
2020-04-27 19:06:55 +0200

    Changed brightness with variable

--- populate.py
+++ populate.py
@@ -6,21 +6,24 @@ import time
 black = (0, 0, 0)
 blue = (0, 0, 16)
 
+b = 16 # brightness
+n = np.n
+
 def change_led(np):
-    led = randint(0, (np.n - 1))
-    speed = randint(30, 60)
+    led = randint(0, (n - 1))
+    speed = randint(15, 90)
     if np[led] == blue:
-        for i in range(0, 17):
-            i = 16 - i
+        for i in range(b + 1):
+            i = b - i
             np[led] = (0, 0, i)
             np.write()
             time.sleep_ms(speed)
     else:
-        for i in range(0, 17):
+        for i in range(b + 1):
             np[led] = (0, 0, i)
             np.write()
             time.sleep_ms(speed)
-    print("Led: " + str(led) + " is changed)
+    print("Led: " + str(led) + " is changed")
     time.sleep_ms(randint(60, 720))
 
 while True:
